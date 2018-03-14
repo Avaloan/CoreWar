5a998d182f1ada82566032aef25f9149ea399c51
Guillaume QUERRE <gquerre@student.42.fr>
2018-03-14 04:28:29 +0100

    3rd try

--- VM/environment/envi.h
+++ VM/environment/envi.h
@@ -6,19 +6,28 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 01:43:22 by gquerre           #+#    #+#             */
-/*   Updated: 2018/03/14 01:51:50 by gquerre          ###   ########.fr       */
+/*   Updated: 2018/03/14 04:25:54 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define CYCLE_TO_DIE
-#define CYCLE_DELTA
-#define NBR_LIVE
-#define MAX_CHECKS
 
-#define	MAX_PLAYERS		4
+/*
+* 
+ */
+
 #define	MEM_SIZE		(4 * 1024)
 #define IDX_MOD			(MEM_SIZE / 8)
 #define REG_NUMBER		16
+
+/*
+** 
+*/
+
+#define	MAX_PLAYERS		4
+#define CYCLE_TO_DIE	1536
+#define CYCLE_DELTA
+#define NBR_LIVE
+#define MAX_CHECKS
 
 #define T_REG			1
 #define T_DIR			2
@@ -29,6 +38,12 @@
 #define COMMENT_LENGTH	2048
 #define COREWAR			0xea83f3
 
+/*
+** 
+*/
+
+
+#define CYCLE_TO_DIE
 #define
 #define
 #define
--- VM/environment/ft_create_arena.c
+++ VM/environment/ft_create_arena.c
@@ -6,15 +6,30 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 02:07:51 by gquerre           #+#    #+#             */
-/*   Updated: 2018/03/14 02:15:03 by gquerre          ###   ########.fr       */
+/*   Updated: 2018/03/14 04:25:57 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include ".h"
 
-int	ft_create_arena(t_env *e)
+int	ft_init_players(t_env *e, int nb_of_player)
 {
-	if (!(e->arena == (char *)(ft_memalloc((char) * (MEM_SIZE + 1)))))
-		return (-1);
+	int	i;
+
+	i = 0;
+	while (i < nb_of_players)
+	{
+		ft_init_players(e, i);
+		ft_import_champ(e, i);
+	}
 
 }
+
+int	ft_create_arena(t_env *e)
+{
+	if (!(e->arena = (ft_memalloc(sizeof(unsigned char) * (MEM_SIZE)))))
+		return (0);
+	return (1);
+}
+
+
