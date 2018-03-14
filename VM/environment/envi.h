339bdcad715e2ae23524924c380b6179e99d4d5d
Guillaume QUERRE <gquerre@student.42.fr>
2018-03-14 02:26:11 +0100

    1st try

---
+++ VM/environment/envi.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   envi.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/14 01:43:22 by gquerre           #+#    #+#             */
+/*   Updated: 2018/03/14 01:51:50 by gquerre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define CYCLE_TO_DIE
+#define CYCLE_DELTA
+#define NBR_LIVE
+#define MAX_CHECKS
+
+#define	MAX_PLAYERS		4
+#define	MEM_SIZE		(4 * 1024)
+#define IDX_MOD			(MEM_SIZE / 8)
+#define REG_NUMBER		16
+
+#define T_REG			1
+#define T_DIR			2
+#define T_IND			3
+#define T_LAB			8
+
+#define	NAME_LENGTH		128
+#define COMMENT_LENGTH	2048
+#define COREWAR			0xea83f3
+
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
