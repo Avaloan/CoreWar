1c03c5158e671a866848d0da219df44f25b2bc01
Guillaume QUERRE <gquerre@student.42.fr>
2018-03-14 06:16:27 +0100

    End_1403

--- VM/environment/envi.h
+++ VM/environment/envi.h
@@ -6,21 +6,21 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 01:43:22 by gquerre           #+#    #+#             */
-/*   Updated: 2018/03/14 04:25:54 by gquerre          ###   ########.fr       */
+/*   Updated: 2018/03/14 06:13:24 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 /*
-* 
- */
+**
+*/
 
 #define	MEM_SIZE		(4 * 1024)
 #define IDX_MOD			(MEM_SIZE / 8)
 #define REG_NUMBER		16
 
 /*
-** 
+** ENVIRONMENT
 */
 
 #define	MAX_PLAYERS		4
@@ -32,16 +32,47 @@
 #define T_REG			1
 #define T_DIR			2
 #define T_IND			3
-#define T_LAB			8
+#d efine T_LAB			8
 
 #define	NAME_LENGTH		128
 #define COMMENT_LENGTH	2048
 #define COREWAR			0xea83f3
 
+typedef struct			s_env
+{
+	int					nb_of_pl;
+	int					nb_of_check;
+	int					cycle;
+	char				carry;
+	void				*(op_code)(s_env *, int, int, int);
+	t_process			*pc_list; 
+	void				*arena;
+}						t_env
+
 /*
-** 
+** PLAYERS
 */
 
+typedef struct			s_player
+{
+	int					num_player;
+	int					lives_during_periode;
+	int					lives_total;
+	int					
+}						t_player;
+
+/*
+** PROCESS
+*/
+
+typedef struct			s_process
+{
+	int					pc;
+	int					reg[16];
+	int					waiting;
+	int					live_during_periode;
+	
+}						t_process
 
 #define CYCLE_TO_DIE
 #define
--- VM/environment/ft_create_arena.c
+++ VM/environment/ft_create_arena.c
@@ -6,21 +6,27 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 02:07:51 by gquerre           #+#    #+#             */
-/*   Updated: 2018/03/14 04:25:57 by gquerre          ###   ########.fr       */
+/*   Updated: 2018/03/14 06:13:22 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include ".h"
 
-int	ft_init_players(t_env *e, int nb_of_player)
+int	ft_init_players(t_env *e)
 {
 	int	i;
 
 	i = 0;
-	while (i < nb_of_players)
+	while (i < e->nb_of_pl)
 	{
-		ft_init_players(e, i);
-		ft_import_champ(e, i);
+		ft_init_process(e, i);//creer les cartes d'identite des process initiaux (et les suivants (fork) ?) - 1 process par joueur
+		//cartes organisees en liste chainee;
+		//-CONTIENT	&&	-Initializaton :
+		//PC		 	MEM_SIZE / e->nb_of_pl * (num_player - 1) ;
+		//R1			0 - num_player (ex: Player1, 0 - 1 = FFFF);
+		//R2-R16		0;
+
+		ft_import_champ(e, i);//ecrire les champions dans la VM
 	}
 
 }
@@ -29,6 +35,9 @@ int	ft_create_arena(t_env *e)
 {
 	if (!(e->arena = (ft_memalloc(sizeof(unsigned char) * (MEM_SIZE)))))
 		return (0);
+	if (!(e->players = (ft_memalloc(sizeof(t_player *) * e->nb_of_pl))))
+		return (0);
+
 	return (1);
 }
 
