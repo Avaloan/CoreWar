372c4f3033e7410a10170d2a283a6ea4c0c81ce0
Felicien DIDELOT <fdidelot@student.42.fr>
2018-03-19 06:02:18 +0100

    global structure ready

--- asm_dir/srcs/main.c
+++ asm_dir/srcs/main.c
@@ -6,11 +6,38 @@
 /*   By: fdidelot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 04:02:28 by fdidelot          #+#    #+#             */
-/*   Updated: 2018/03/14 06:01:08 by fdidelot         ###   ########.fr       */
+/*   Updated: 2018/03/19 05:53:29 by fdidelot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
+
+t_op    g_op_tab[17] =
+{
+	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 0},
+	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0},
+	{"st", 2, {T_REG, T_IND | T_REG}, 3, 5, "store", 1, 0},
+	{"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1, 0},
+	{"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction", 1, 0},
+	{"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 6,
+	 "et (and  r1, r2, r3   r1&r2 -> r3", 1, 0},
+	{"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 6,
+	 "ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0},
+	{"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6,
+	 "ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0},
+	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1},
+	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25,
+	 "load index", 1, 1},
+	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25,
+	 "store index", 1, 1},
+	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1},
+	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0},
+	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50,
+	 "long load index", 1, 1},
+	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},
+	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0},
+	{0, 0, {0}, 0, 0, 0, 0, 0}
+};
 
 int	check_s_ponkt(char *str)
 {
@@ -32,12 +59,13 @@ int	main(int ac, char **av)
 		ft_perror("Mets un joueur ouech.\n");
 	if (!(fd = open(av[1], O_RDONLY)))
 		ft_perror("Open() failed.\n");
-	printf("fd = %d\n", off_set);
 	off_set = lseek(fd, 0, SEEK_END);
-	printf("fd = %d", off_set);
+	if (off_set > CHAMP_MAX_SIZE)
+		ft_perror("Champ too fat, go on a diet.\n");
 	stock = (char *)ft_memalloc(sizeof(char) * off_set);
 	lseek(fd, 0, SEEK_SET);
 	read(fd, stock, off_set);
 	printf("Player :\n%s\n", stock);
+	printf("Test = %s\n", g_op_tab[1].name);
 	return (0);
 }
