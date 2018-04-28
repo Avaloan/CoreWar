0439ca025a87279696f689a82e78469be7d5319a
Felicien DIDELOT <fdidelot@student.42.fr>
2018-04-28 06:33:02 +0200

    recover ?

---
+++ asm_dir/srcs/replace_label.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace_label.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdidelot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/27 01:19:01 by fdidelot          #+#    #+#             */
+/*   Updated: 2018/04/28 06:08:40 by fdidelot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+void	put_label_into_buff(unsigned char *buff, int pos, unsigned int val, int size_dir)
+{
+	if (size_dir == 2)
+	{
+		number_to_hex_size_two(val, buff + pos);
+	}
+	else if (size_dir == 4)
+	{
+		number_to_hex(val, buff + pos);
+	}
+	else
+		ft_perror("WHATTHEFYCYJEDKEDGIGIUGWDPWDGOOUHDOBOUDJWOJDNWO\n");
+}
+
+int	replace_label(t_env *e)
+{
+	t_label	*tmp;
+	t_label	*stock;
+	int		label_exist;
+
+	stock = e->stock_label;
+	while (stock)
+	{
+		label_exist = 0;
+		tmp = e->lst_label;
+		while (tmp)
+		{
+			if (ft_strcmp(stock->name, tmp->name) == 0)
+			{
+				put_label_into_buff(e->player_buff, stock->placement + stock->deca, tmp->placement - stock->placement, stock->dir_size);
+				label_exist = 1;
+			}
+			tmp = tmp->next;
+		}
+		if (!label_exist)
+			return (0);
+		stock = stock->next;
+	}
+	return (1);
+}
