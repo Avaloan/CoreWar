202e7fc3b4d9f1108f67f4fdc11bd7e713acc17f
Guillaume QUERRE <gquerre@student.42.fr>
2018-04-30 13:55:18 +0200

    Final_VM

--- VM/visual/ft_maj_visu.c
+++ VM/visual/ft_maj_visu.c
@@ -6,7 +6,7 @@
 /*   By: gquerre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 02:19:28 by gquerre           #+#    #+#             */
-/*   Updated: 2018/04/28 06:40:26 by gquerre          ###   ########.fr       */
+/*   Updated: 2018/04/30 13:54:04 by gquerre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	ft_maj_visu(t_env *e)
 {
 	if (ft_fill_arena(e) == 0 || ft_fill_info(e) == 0)
 		return (0);
+	move(e->vi->my, 0);
+	clrtoeol();
 	wrefresh(e->vi->arena);
 	wrefresh(e->vi->info);
+	refresh();
 	if (e->cycles == 1)
 		getch();
 	return (1);
